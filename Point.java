6fffa408b94f18c9a72b49eae992a02e6f47a15d
HollowStrigiform <84941037+HollowStrigiform@users.noreply.github.com>
2021-05-27 22:25:35 -0500

    Added documentation
    Made a few comments for clarity

--- Point.java
+++ Point.java
@@ -1,12 +1,13 @@
-
-public class Point
+public class Point //A point which contains x and y coordinates that will later form a connected line
 {
-	int x, y;
+	int x, y; //Coordinates in integer form
 	public Point(int x, int y)
 	{
 		this.x = x;
 		this.y = y;
 	}
+	
+	//Gives exact x and y location of the point
 	public int giveX()
 	{
 		return x;
